fa933d79be08732514fc5a4109af2e37d585284c
Giuliana Taylor <giulswatson@gmail.com>
2019-06-16 20:42:26 -0400

    Calculate approximate length of an arc given it's starting distance from zero

--- src/Encoder.hs
+++ src/Encoder.hs
@@ -1,6 +1,18 @@
-module Encoder
-    ( encode
-    ) where
+module Encoder where
+
+import Numeric.Tools.Integration
 
 encode :: Int -> Int
 encode x = x
+
+getCircumference :: Double -> Double
+getCircumference diameter = pi * diameter
+
+getAproximateLengthOfFirstSpiral :: Double -> Int
+getAproximateLengthOfFirstSpiral radius =
+    let lowerLimit = 0
+        upperLimit = pi/4
+        l x = radius * sqrt((tan x)^2 + 1)
+        res = quadRomberg defQuad (lowerLimit, upperLimit) l
+        arcLength = 8 * quadBestEst res
+    in round arcLength
--- test/EncoderSpec.hs
+++ test/EncoderSpec.hs
@@ -9,4 +9,17 @@ main = hspec $ do
         it "should be the identity function" $ do
             encode 1 `shouldBe` 1
 
+        it "should calculate the circumference" $ do 
+            let diameter = 2.1
+                expectedCircumference = 6.5973445725385655
+
+            getCircumference diameter `shouldBe` expectedCircumference
+
+        it "should caluclate an approximation of the length of the first spiral" $ do
+            let radius = 1
+                expectedArcLength = 7
+
+            getAproximateLengthOfFirstSpiral radius `shouldBe` expectedArcLength
+
+
         

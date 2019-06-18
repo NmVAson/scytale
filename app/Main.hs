00b02e2aed67079f865de58a9a48fc71d86b8178
Giuliana Taylor <giulswatson@gmail.com>
2019-06-17 22:46:41 -0400

    Encode and decode a message from user input as main function

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,12 @@ module Main where
 import Encoder
 
 main :: IO ()
-main = putStrLn "Do something here"
+main = do
+    putStrLn "Hello, send me a message!"  
+    message <- getLine
+
+    encodedMessage <- encode message 1
+    putStrLn ("Encoded: " ++ encodedMessage)
+    
+    let decodedMessage = decode encodedMessage 1
+    putStrLn ("And here it is decoded again: " ++ decodedMessage) 
